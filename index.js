e660d71a375a39216c88bf9a6076551d3d554a88
Defu_Hackinosh <390135963@qq.com>
2022-07-17 20:33:38 +0800

    feat: 开发者工具菜单

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ app.on('ready', () => {
         width: 800, height: 600
     });
     win.loadURL(url.format({
-        pathname: path.resolve(__dirname,'./html/main.html'),
+        pathname: path.resolve(__dirname, './html/main.html'),
         protocol: 'file:',
-        slashes:true
+        slashes: true
     }))
 
     //定义菜单
@@ -20,15 +20,36 @@ app.on('ready', () => {
 })
 
 // 顶部菜单模板
-const menuTemplate = [{
-    label: '文件',
-    submenu: [
-        {label: '新增信息'},
-        {label: '清空信息'},
-        {label: '退出', 
-            accelerator: process.platform == 'darwin'? 'Command+Q': 'Ctrl+Q',
-            click: () => {
-            app.quit();
-        }}
-    ]
-}]
+const menuTemplate = [
+    {
+        label: '文件',
+        submenu: [
+            { label: '新增信息' },
+            { label: '清空信息' },
+            {
+                label: '退出',
+                accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+                click: () => {
+                    app.quit();
+                }
+            }
+        ]
+    }, {
+        label: '开发中工具',
+        submenu: [
+            {
+                label: '打开/关闭',
+                click: (item, focuseWindow) => {
+                    focuseWindow.toggleDevTools()
+                },
+                accelerator: process.platform == 'darwin' ? 'Command+I' : 'Ctrl+I'
+            },
+            {
+                label: '刷新一下',
+                role: 'reload',
+                accelerator: process.platform == 'darwin' ? 'Command+F5' : 'Ctrl+F5'
+            }
+        ]
+    }
+]
+
